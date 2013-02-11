fb646990f11154ba28ada2fa3c08a39f577027e1
Colin Nederkoorn <colin.nederkoorn@gmail.com>
2013-02-10 22:34:37 -0500

    Added rbenv

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,11 +1,6 @@
 #!/usr/bin/env bash
 apt-get -y update
-apt-get -y install build-essential zlib1g-dev libssl-dev libreadline-gplv2-dev libyaml-dev
-cd /tmp
-wget ftp://ftp.ruby-lang.org/pub/ruby/1.9/ruby-1.9.3-p385.tar.gz
-tar -xvzf ruby-1.9.3-p385.tar.gz
-cd ruby-1.9.3-p385/
-./configure --prefix=/usr/local
-make
-make install
+apt-get -y install build-essential zlib1g-dev libssl-dev libreadline-gplv2-dev libyaml-dev libxml2-dev libxslt1-dev
+apt-get -y install ruby 
+apt-get -y install ruby-dev
 gem install chef ruby-shadow --no-ri --no-rdoc
