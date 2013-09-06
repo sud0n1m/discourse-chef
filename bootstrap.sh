74e98f9214ae6ee23e00fc4232717b82e9cacb3b
Gaurish Sharma <contact@gaurishsharma.com>
2013-09-06 12:27:17 +0530

    Upgrade ruby patch level & rubygems

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,19 +1,25 @@
 #!/usr/bin/env bash
+#update the update the system & install latest updates
 apt-get -y update
+apt-get -y upgrade
+#install ruby dependancies
 apt-get -y install build-essential zlib1g-dev libssl-dev libreadline-gplv2-dev libyaml-dev libxml2-dev libxslt1-dev
 
 # For Ubuntu 12.10 you can just install ruby from the package
 # apt-get install ruby
 # apt-get install ruby-dev
 
-# INSTALL RUBY
+# INSTALL RUBY from source
 cd /tmp
-wget http://ftp.ruby-lang.org/pub/ruby/1.9/ruby-1.9.3-p385.tar.gz
-tar -xvzf ruby-1.9.3-p385.tar.gz
-cd ruby-1.9.3-p385/
+wget https://ftp.ruby-lang.org/pub/ruby/1.9/ruby-1.9.3-p448.tar.gz
+tar -xvzf ruby-1.9.3-p448.tar.gz
+cd ruby-1.9.3-p448/
 ./configure --prefix=/usr/local
 make
 make install
 
+# Update rubygems
+gem update --system
+
 # INSTALL CHEF
-gem install chef ruby-shadow --no-ri --no-rdoc
+gem install chef ruby-shadow --no-document
